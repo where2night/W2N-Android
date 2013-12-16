20a891a75bb9a7b2c942e9a89ec3b843341c3925
Ismaibz <ismael.requena.andreu@gmail.com>
2013-12-16 17:25:54 +0100

    Añadida funcionalidad para llegar a la MainActivity

--- Where2Night/src/com/where2night/activities/LoginEmailActivity.java
+++ Where2Night/src/com/where2night/activities/LoginEmailActivity.java
@@ -6,7 +6,10 @@ import com.where2night.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginEmailActivity extends Activity {
 
@@ -14,6 +17,17 @@ public class LoginEmailActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login_email);
+		
+		 Button btnLoginEmail = (Button) findViewById(R.id.btnLogin);
+	        btnLoginEmail.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					Intent i = new Intent(getApplicationContext(), MainActivity.class);
+					startActivity(i);
+					
+				}
+			});
 	}
 
 	@Override
