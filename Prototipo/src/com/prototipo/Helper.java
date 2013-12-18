ca70459ad1f8ba64a5fbab21148f6353542a2971
Ismaibz <ismael.requena.andreu@gmail.com>
2013-12-18 03:00:05 +0100

    Prototipo - Conexion erronea con el servidor
    Puede que exista algún problema en la API

---
+++ Prototipo/src/com/prototipo/Helper.java
@@ -0,0 +1,10 @@
+package com.prototipo;
+
+public class Helper {
+	public final static String INSTAGRAM_API_KEY = "fc8041d4af1544a2939c3f5a9a1ef8cf";
+	public final static String BASE_API_URL = "http://95.85.33.106:3000/explore/pop/";
+	
+	public static String getRecentUrl(){
+		return BASE_API_URL; 
+	}
+}
